fde4702b00a17fa0c9a97101de02a6dfeec3e52d
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-02-27 19:53:47 -0500

    RPS Game

--- app.js
+++ app.js
@@ -3,16 +3,7 @@ let player2Hand = '';
 
 //Player Hand
 const pickHand(hand, player) {
-   if (player === 1) {
-    player1Hand = hand;
-    document.getElementById('player1-hand').src = hand + "http://musicuentos.com/wp-content/blogs.dir/1/files/2015/10/rock-paper-scissors-156171_1280.png";
-
-   } else {
-    player2Hand = hand;
-    document.getElementById('player2-hand').src = hand + "http://musicuentos.com/wp-content/blogs.dir/1/files/2015/10/rock-paper-scissors-156171_1280.png";
-   }
-   confirmWinner();
-}
+  
 
 
 
